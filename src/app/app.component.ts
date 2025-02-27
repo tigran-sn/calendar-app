99c16c239fda736ca3386619d06be07aa2a52ed1
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2025-02-27 23:12:57 +0400

    - Added Header component - Restructured AppComponent to use Header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
+import { HeaderComponent } from './shared/components/header/header.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbarModule],
+  imports: [RouterOutlet, MatToolbarModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'Calendar';
-}
+export class AppComponent {}
---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterLink],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent {
+  title = 'Calendar';
+}
